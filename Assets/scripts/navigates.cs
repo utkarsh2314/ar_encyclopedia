4113e950124405709b8f2261082489b5d0daa0a2
utkarsh2314 <utkarshukla2003@gmail.com>
2023-06-02 21:07:04 +0530

    All Scenes and navigation script added

---
+++ Assets/scripts/navigates.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class navigate : MonoBehaviour
+{
+    public void main_page ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void animal_page ()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void galaxy_page ()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void about_page ()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void quit_it ()
+    {
+        Debug.Log("QUIT");
+        Application.Quit(0);
+    }
+}
